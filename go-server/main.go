c0957b1b74d1fb911e6d2ef8156c6d3f5d8495e6
Swastik Sharma <swastkk@gmail.com>
2023-06-27 14:51:06 +0530

    Add hello template in go-server
    Signed-off-by: Swastik Sharma <swastkk@gmail.com>

--- go-server/main.go
+++ go-server/main.go
@@ -4,8 +4,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/thedevsaddam/renderer"
 )
 
+var rnd *renderer.Render
+
+// Here we have initialised a func init which will initate the opts variable.
+func init() {
+	opts := renderer.Options{
+		ParseGlobPattern: "./static/*.html",
+	}
+
+	rnd = renderer.New(opts)
+}
+
+// Defined the function named helloHandler which will render the page and the methods
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found", http.StatusNotFound)
@@ -15,19 +29,19 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Method not supported", http.StatusNotFound)
 		return
 	}
-	fmt.Fprintf(w, "hello!")
+	rnd.HTML(w, http.StatusOK, "hello", nil)
 }
 
+// Defined formHandler function which will handle the form route
 func formHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm() err: %v ", err)
 		return
 	}
-	fmt.Fprintf(w, "Post request succesfull! \n")
-	name := r.FormValue("name")
-	email := r.FormValue("email")
-	fmt.Fprintf(w, "Name: %s\n", name)
-	fmt.Fprintf(w, "Email: %s\n", email)
+	// fmt.Fprintf(w, "Post request succesfull! \n")
+	// name := r.FormValue("name")
+	// email := r.FormValue("email")
+	rnd.HTML(w, http.StatusOK, "form", nil)
 }
 
 func main() {
@@ -35,7 +49,7 @@ func main() {
 	http.Handle("/", fileserver)
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
-
+	// rnd = renderer.New(opts)
 	fmt.Printf("Server starting on port 7000\n")
 	if err := http.ListenAndServe(":7000", nil); err != nil {
 		log.Fatal(err)
