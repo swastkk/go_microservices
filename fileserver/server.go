12040206214aff211f4c1f3bdec3853d788fc938
Swastik Sharma <swastkk@gmail.com>
2023-06-27 17:17:47 +0530

    Try implementing FileServer with detailed code!
    Signed-off-by: Swastik Sharma <swastkk@gmail.com>

--- fileserver/server.go
+++ fileserver/server.go
@@ -7,6 +7,18 @@ import (
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./public/")))
-	fmt.Printf("Server is up and running...")
+	fmt.Printf("FileServer is up and running at PORT 5000")
 	http.ListenAndServe(":5000", nil)
 }
+
+// Here is the detailed implementation of the Code that is done above!
+
+// func main() {
+// 	dir := http.Dir("./public/")
+// 	fmt.Printf("This is the dir: %s\n", dir)
+// 	dirHandler := http.FileServer(dir)
+// 	http.Handle("/", dirHandler)
+// 	fmt.Print("Fileserver is up and running at port 5000...\n ")
+// 	http.ListenAndServe(":5000", nil)
+
+// }
