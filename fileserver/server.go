d9d6e9df99c06c2a1bb1e64f5c9c027971a4f874
Swastik Sharma <swastkk@gmail.com>
2023-06-30 13:21:05 +0530

    Add longer code as well for fileserver
    Signed-off-by: Swastik Sharma <swastkk@gmail.com>

--- fileserver/server.go
+++ fileserver/server.go
@@ -5,20 +5,23 @@ import (
 	"net/http"
 )
 
+// Here is the detailed implementation of the FileServer Code.
+
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./public/")))
-	fmt.Printf("FileServer is up and running at PORT 5000")
+	dir := http.Dir("./public/")
+	fmt.Printf("This is the dir: %s\n", dir)
+	dirHandler := http.FileServer(dir)
+	http.Handle("/", dirHandler)
+	fmt.Print("Fileserver is up and running at port 5000...\n ")
 	http.ListenAndServe(":5000", nil)
+
 }
 
-// Here is the detailed implementation of the Code that is done above!
-
+// Can implement the code in the shorter format like this! Both works same.
 // func main() {
-// 	dir := http.Dir("./public/")
-// 	fmt.Printf("This is the dir: %s\n", dir)
-// 	dirHandler := http.FileServer(dir)
-// 	http.Handle("/", dirHandler)
-// 	fmt.Print("Fileserver is up and running at port 5000...\n ")
+// 	http.Handle("/", http.FileServer(http.Dir("./public/")))
+// 	fmt.Printf("FileServer is up and running at PORT 5000")
 // 	http.ListenAndServe(":5000", nil)
-
 // }
+
+// It took around 03:38:71(Min:Sec:MS) for sharing 1.21GB mp4 format file.
